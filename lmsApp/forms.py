54a136bda24c07c22877177fa879a06008e5204e
Bui Khanh Huyen <buikhanhhuyen2903@gmail.com>
2024-01-07 22:34:16 +0700

    update

---
+++ lmsApp/forms.py
@@ -0,0 +1,284 @@
+from datetime import datetime
+from random import random
+from secrets import choice
+from sys import prefix
+from unicodedata import category
+from django import forms
+from numpy import require
+from lmsApp import models
+
+from django.contrib.auth.forms import UserCreationForm,PasswordChangeForm, UserChangeForm
+from django.contrib.auth.models import User
+import datetime
+
+
+class SaveUser(UserCreationForm):
+    ID = forms.IntegerField()
+    first_name = forms.CharField(max_length=250, help_text="The First name field is required.")
+    middle_name = forms.CharField(max_length=250, required= False)
+    last_name = forms.CharField(max_length=250, help_text="The Last name field is required.")
+    username = forms.CharField(max_length=250,help_text="The Username field is required.")
+    birth_date = forms.DateField()
+    gender = forms.CharField(max_length=250)
+    contact = forms.CharField(max_length=250)
+    email = forms.CharField(max_length=250, help_text="The Email field is required.")
+    address = forms.Textarea()
+    is_active = forms.CharField(max_length=2)
+    password1 = forms.CharField(max_length=250)
+    password2 = forms.CharField(max_length=250)
+
+    class Meta:
+        model = models.CustomUser
+        fields = ('ID', 'first_name', 'middle_name', 'last_name', 'username', 'birth_date', 'gender', 'contact', 'email', 'address', 'is_active', 'password1', 'password2',)
+
+class UpdateProfile(forms.ModelForm):
+    username = forms.CharField(max_length=250,help_text="The Username field is required.")
+    email = forms.EmailField(max_length=250,help_text="The Email field is required.")
+    first_name = forms.CharField(max_length=250,help_text="The First Name field is required.")
+    middle_name = forms.CharField(max_length=250)
+    last_name = forms.CharField(max_length=250,help_text="The Last Name field is required.")
+    birth_date = forms.DateField()
+    gender = forms.CharField(max_length=20)
+    contact = forms.CharField(max_length=250)
+    address = forms.Textarea()
+
+    class Meta:
+        model = models.CustomUser
+        fields = ('email', 'username','first_name', 'middle_name', 'last_name', 'birth_date', 'gender', 'contact', 'address', )
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        try:
+            user = models.CustomUser.objects.exclude(id=self.cleaned_data['id']).get(email = email)
+        except Exception as e:
+            return email
+        raise forms.ValidationError(f"This mail is already exists/taken")
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            user = models.CustomUser.objects.exclude(id=self.cleaned_data['id']).get(username = username)
+        except Exception as e:
+            return username
+        raise forms.ValidationError(f"The username is already exists/taken")
+
+class UpdateUser(UserChangeForm):
+    first_name = forms.CharField(max_length=250, help_text="The First name field is required.")
+    middle_name = forms.CharField(max_length=250, required= False)
+    last_name = forms.CharField(max_length=250, help_text="The Last name field is required.")
+    username = forms.CharField(max_length=250,help_text="The Username field is required.")
+    birth_date = forms.DateField()
+    gender = forms.CharField(max_length=250)
+    contact = forms.CharField(max_length=250)
+    email = forms.CharField(max_length=250, help_text="The Email field is required.")
+    address = forms.Textarea()
+    
+    class Meta:
+        model = models.CustomUser
+        fields = ('first_name', 'middle_name', 'last_name', 'username', 'birth_date', 'gender', 'contact', 'email', 'address',)
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        try:
+            user = models.CustomUser.objects.exclude(id=self.cleaned_data['id']).get(email = email)
+        except Exception as e:
+            return email
+        raise forms.ValidationError(f"This mail is already exists/taken")
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            user = models.CustomUser.objects.exclude(id=self.cleaned_data['id']).get(username = username)
+        except Exception as e:
+            return username
+        raise forms.ValidationError(f"This username is already exists/taken")
+
+class UpdatePasswords(PasswordChangeForm):
+    old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control form-control-sm rounded-0'}), label="Mật khẩu hiện tại")
+    new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control form-control-sm rounded-0'}), label="Mật khẩu mới")
+    new_password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control form-control-sm rounded-0'}), label="Xác nhận lại mật khẩu")
+    class Meta:
+        model = models.CustomUser
+        fields = ('old_password','new_password1', 'new_password2')
+    def clean_old_password(self):
+        if not self.instance.check_password(self.cleaned_data['old_password']):
+            raise forms.ValidationError(f"Password is Incorrect")
+
+class SaveCategory(forms.ModelForm):
+    name = forms.CharField(max_length=250)
+    description = forms.Textarea()
+    status = forms.CharField(max_length=2)
+
+    class Meta:
+        model = models.Category
+        fields = ('name', 'description', 'status', )
+
+    def clean_name(self):
+        id = self.data['id'] if (self.data['id']).isnumeric() else 0
+        name = self.cleaned_data['name']
+        try:
+            if id > 0:
+                category = models.Category.objects.exclude(id = id).get(name = name, delete_flag = 0)
+            else:
+                category = models.Category.objects.get(name = name, delete_flag = 0)
+        except:
+            return name
+        raise forms.ValidationError("Category Name already exists.")
+"""
+class SaveSubCategory(forms.ModelForm):
+    category = forms.CharField(max_length=250)
+    name = forms.CharField(max_length=250)
+    description = forms.Textarea()
+    status = forms.CharField(max_length=2)
+
+    class Meta:
+        model = models.SubCategory
+        fields = ('category', 'name', 'description', 'status', )
+
+    def clean_category(self):
+        cid = int(self.data['category']) if (self.data['category']).isnumeric() else 0
+        try:
+            category = models.Category.objects.get(id = cid)
+            return category
+        except:
+            raise forms.ValidationError("Invalid Category.")
+
+    def clean_name(self):
+        id = int(self.data['id']) if (self.data['id']).isnumeric() else 0
+        cid = int(self.data['category']) if (self.data['category']).isnumeric() else 0
+        name = self.cleaned_data['name']
+        try:
+            category = models.Category.objects.get(id = cid)
+            if id > 0:
+                sub_category = models.SubCategory.objects.exclude(id = id).get(name = name, delete_flag = 0, category = category)
+            else:
+                sub_category = models.SubCategory.objects.get(name = name, delete_flag = 0, category = category)
+        except:
+            return name
+        raise forms.ValidationError("Sub-Category Name already exists on the selected Category.")
+"""
+class SaveBook(forms.ModelForm):
+    id = forms.IntegerField()
+    category = forms.CharField(max_length=250)
+    title = forms.CharField(max_length=250)
+    description = forms.Textarea()
+    publisher = forms.Textarea()
+    date_published = forms.DateField()
+    quantity = forms.IntegerField()
+    status = forms.CharField(max_length=2)  
+    
+    
+    class Meta:
+        model = models.Books
+        fields = ('id', 'category', 'title', 'description', 'publisher', 'date_published', 'quantity', 'status', )
+
+    def clean_category(self):
+        scid = int(self.data['category']) if (self.data['category']).isnumeric() else 0
+        try:
+            category = models.Category.objects.get(id = scid)
+            return category
+        except:
+            raise forms.ValidationError("Invalid Category.")
+    """
+    def clean_isbn(self):
+        id = int(self.data['id']) if (self.data['id']).isnumeric() else 0
+        isbn = self.cleaned_data['isbn']
+        try:
+            if id > 0:
+                book = models.Books.objects.exclude(id = id).get(isbn = isbn, delete_flag = 0)
+            else:
+                book = models.Books.objects.get(isbn = isbn, delete_flag = 0)
+        except:
+            return isbn
+        raise forms.ValidationError("ISBN already exists on the Database.")"""
+"""  
+class SaveMember(forms.ModelForm):
+    ID = forms.IntegerField()
+    first_name = forms.CharField(max_length=250, help_text="The First name field is required.")
+    middle_name = forms.CharField(max_length=250, required= False)
+    last_name = forms.CharField(max_length=250, help_text="The Last name field is required.")
+    username = forms.CharField(max_length=250,help_text="The Username field is required.")
+    birth_date = forms.DateField()
+    gender = forms.CharField(max_length=250)
+    contact = forms.CharField(max_length=250)
+    email = forms.CharField(max_length=250, help_text="The Email field is required.")
+    address = forms.Textarea()
+    status = forms.CharField(max_length=2)
+    password = forms.CharField(max_length=250, help_text="The Password field is required.")
+    
+
+    class Meta:
+        model = models.Member
+        fields = ('first_name', 'middle_name', 'last_name', 'username', 'birth_date', 'gender', 'contact', 'email', 'address', 'status', 'password', )
+
+    def clean_code(self):
+        id = int(self.data['id']) if (self.data['id']).isnumeric() else 0
+        code = self.cleaned_data['id']
+        try:
+            if id > 0:
+                book = models.Member.objects.exclude(id = id).get(code = code, delete_flag = 0)
+            else:
+                book = models.Member.objects.get(code = code, delete_flag = 0)
+        except:
+            return code
+        raise forms.ValidationError("Member Id already exists on the Database.")
+
+class UpdateMember(UserChangeForm):
+    first_name = forms.CharField(max_length=250,help_text="The First Name field is required.")
+    middle_name = forms.CharField(max_length=250, required= False)
+    last_name = forms.CharField(max_length=250,help_text="The Last Name field is required.")
+    username = forms.CharField(max_length=250,help_text="The Username field is required.")
+    gender = forms.CharField(max_length=250)
+    contact = forms.CharField(max_length=250)
+    email = forms.EmailField(max_length=250,help_text="The Email field is required.")
+    address = forms.CharField(max_length=250)
+    status = forms.CharField(max_length=2)
+    password = forms.CharField(max_length=250, help_text="The Password field is required.")
+    
+    
+    class Meta:
+        model = models.Member
+        fields = ('first_name', 'middle_name', 'last_name', 'username', 'gender', 'contact', 'email', 'address', 'status', 'password', )
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        try:
+            user = models.Member.objects.exclude(id=self.cleaned_data['id']).get(email = email)
+        except Exception as e:
+            return email
+        raise forms.ValidationError(f"The {user.email} mail is already exists/taken")
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            user = models.Member.objects.exclude(id=self.cleaned_data['id']).get(username = username)
+        except Exception as e:
+            return username
+        raise forms.ValidationError(f"The {user.username} mail is already exists/taken")
+"""
+class SaveBorrow(forms.ModelForm):
+    member = forms.CharField(max_length=250)
+    book = forms.CharField(max_length=250)
+    borrowing_date = forms.DateField()
+    return_date = forms.DateField()
+    status = forms.CharField(max_length=2)
+
+    class Meta:
+        model = models.Borrow
+        fields = ('member', 'book', 'borrowing_date', 'return_date', 'status', )
+
+    def clean_member(self):
+        member = int(self.data['member']) if (self.data['member']).isnumeric() else 0
+        try:
+            member = models.Member.objects.get(id = member)
+            return member
+        except:
+            raise forms.ValidationError("Invalid member.")
+            
+    def clean_book(self):
+        book = int(self.data['book']) if (self.data['book']).isnumeric() else 0
+        try:
+            book = models.Books.objects.get(id = book)
+            return book
+        except:
+            raise forms.ValidationError("Invalid Book.")
---
+++ lmsApp/models.py
@@ -0,0 +1,132 @@
+from distutils.command.upload import upload
+from email.policy import default
+from django.db import models
+from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from PIL import Image
+from django.contrib.auth.models import User, AbstractBaseUser, AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+
+
+
+# Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=250)
+    description = models.TextField(blank=True, null= True)
+    status = models.CharField(max_length=2, choices=(('1','Active'), ('2','Inactive')), default = 1)
+    delete_flag = models.IntegerField(default = 0)
+    date_added = models.DateTimeField(default = timezone.now)
+    
+
+    class Meta:
+        verbose_name_plural = "List of Categories"
+
+    def __str__(self):
+        return str(f"{self.name}")
+
+class Publishers(models.Model):
+    name = models.CharField(max_length=250)
+    status = models.CharField(max_length=2, choices=(('1','Active'), ('2','Inactive')), default = 1)
+    delete_flag = models.IntegerField(default = 0)
+    date_added = models.DateTimeField(default = timezone.now)
+    
+    class Meta:
+        verbose_name_plural = "List of Categories"
+
+    def __str__(self):
+        return str(f"{self.name}")
+    
+class CustomUser(AbstractUser):
+    middle_name = models.CharField(max_length=150, blank=True, null= True)
+    gender = models.CharField(max_length=20, choices=(('Nam','Nam'), ('Nữ','Nữ'), ('Khác', 'Khác')), default = 'Nam')
+    contact = models.CharField(max_length=100)
+    address = models.TextField(blank=True, null= True)
+    birth_date = models.DateField(blank=True, null=True)
+    #is_active = models.IntegerField(max_length=2, choices=(('1','Active'), ('2','Inactive')), default = 1)
+    groups = models.ManyToManyField('auth.Group', related_name='customuser_groups', blank=True)
+    user_permissions = models.ManyToManyField('auth.Permission', related_name='customuser_user_permissions', blank=True)
+    
+
+"""
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete= models.CASCADE)
+    name = models.CharField(max_length=250)
+    description = models.TextField(blank=True, null= True)
+    status = models.CharField(max_length=2, choices=(('1','Active'), ('2','Inactive')), default = 1)
+    delete_flag = models.IntegerField(default = 0)
+    date_added = models.DateTimeField(default = timezone.now)
+    date_created = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        verbose_name_plural = "List of Categories"
+
+    def __str__(self):
+        return str(f"{self.category} - {self.name}")
+"""
+class Books(models.Model):
+    id = models.AutoField(primary_key=True)
+    category = models.ForeignKey(Category, on_delete= models.CASCADE, related_name="category_id_fk")
+    title = models.CharField(max_length=250)
+    description = models.TextField(blank=True, null= True)
+    publisher = models.ForeignKey(Publishers, on_delete= models.CASCADE, related_name="publisher_id_fk")
+    date_published = models.DateTimeField()
+    quantity = models.IntegerField(default = 0)
+    status = models.CharField(max_length=2, choices=(('1','Active'), ('2','Inactive')), default = 1)
+    delete_flag = models.IntegerField(default = 0)
+    date_added = models.DateTimeField(default = timezone.now)
+
+    class Meta:
+        verbose_name_plural = "List of Books"
+
+    def __str__(self):
+        return str(f"{self.ID} - {self.title}")
+
+"""
+class Member(models.Model):
+    ID = models.IntegerField()
+    first_name = models.CharField(max_length=250)
+    middle_name = models.CharField(max_length=250, blank=True, null= True)
+    last_name = models.CharField(max_length=250)
+    username = models.CharField(max_length=250)
+    password = models.CharField(max_length=250)
+    birth_date = models.DateTimeField()
+    gender = models.CharField(max_length=20, choices=(('Nam','Nam'), ('Nữ','Nữ'), ('Khác', 'Khác')), default = 'Nam')
+    contact = models.CharField(max_length=250)
+    email = models.CharField(max_length=250)
+    address = models.TextField(blank=True, null= True)
+    status = models.CharField(max_length=2, choices=(('1','Active'), ('2','Inactive')), default = 1)
+    delete_flag = models.IntegerField(default = 0)
+    date_added = models.DateTimeField(default = timezone.now)
+
+    class Meta:
+        verbose_name_plural = "List of Members"
+
+    def __str__(self):
+        return str(f"{self.id} - {self.first_name}{' '+self.middle_name if not self.middle_name == '' else ''} {self.last_name}")
+
+    def name(self):
+        return str(f"{self.first_name}{' '+self.middle_name if not self.middle_name == '' else ''} {self.last_name}")
+"""
+class Borrow(models.Model):
+    member = models.ForeignKey(CustomUser, on_delete= models.CASCADE, related_name="user_id_fk")
+    borrowing_date = models.DateField()
+    status = models.CharField(max_length=2, choices=(('1','Pending'), ('2','Returned')), default = 1)
+    quantity = models.IntegerField(default = 0)
+    class Meta:
+        verbose_name_plural = "Borrowing Transactions"
+
+    def __str__(self):
+        return str(f"{self.member.id}")
+    
+class Borrow_detail(models.Model):
+    book = models.ForeignKey(Books, on_delete= models.CASCADE, related_name="book_id_fk")
+    borrow = models.ForeignKey(Borrow, on_delete= models.CASCADE, related_name="borrow_id_fk")
+    return_date = models.DateField()
+    class Meta:
+        verbose_name_plural = "Borrowing Transactions"
+
+    def __str__(self):
+        return str(f"{self.book.id}")
+
